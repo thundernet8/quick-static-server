1fba88054fea39c54e519d457bd1fc0169c182bd
wuxueqian <wuxueqian@meituan.com>
2018-03-16 16:41:09 +0800

    add cors

--- src/serve.ts
+++ src/serve.ts
@@ -4,6 +4,12 @@ import * as yargs from "yargs";
 import express = require("express");
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const argv = yargs
   .version(function() {
     return require("../package.json").version;
